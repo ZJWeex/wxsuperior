46eb3706305baa27067e049376ff9a4e67ec1ce4
zhuzijia <873391579@qq.com>
2019-02-13 11:57:35 +0800

    添加web加载进度动画

--- base-weex/src/base-superior/base-superior/setting_agreementWeb.vue
+++ base-weex/src/base-superior/base-superior/setting_agreementWeb.vue
@@ -1,7 +1,13 @@
 <template>
     <div class='wrapper'>
-        <navigation :title="title"/>
-        <web class="web" :src="url"></web>
+        <navigation :title="title" @wxcMinibarLeftButtonClicked="goBack"/>
+        <div class="web">
+            <web ref="webview" class="web" :src="url" 
+                @pagestart="onPageStart" 
+                @pagefinish="onPageFinish" 
+                @error="onError"></web>
+            <div ref="progress" class="progress"></div>
+        </div>
     </div>
 </template>
 
@@ -15,6 +21,7 @@ export default {
     components: { navigation },
     data() {
         return {
+            canGoBack: false,
             url:"",
             title:'隐私权政策'
         }
@@ -33,7 +40,62 @@ export default {
         });
     },
     methods: {
-
+        //返回刷新
+        goBack: function() {
+            if(this.canGoBack == true){
+                var webElement = this.$el('webview');
+                var webview = weex.requireModule('webview');
+                webview.goBack(webElement.ref);
+            }else{
+                navigation.pop({pop:true},event=>{});
+            } 
+        },
+        
+        onPageStart: function(e) {
+            console.log(e.url)
+            this.progressAnimation(0.7);
+        },
+        onPageFinish: function(e) {
+            this.canGoBack = e.canGoBack;
+            this.progressAnimation(1.0);
+            if (e.title) {
+                this.title = e.title;
+                console.log(e.title)
+            }
+        },
+        onError: function(e) {
+            console.log(e.url)
+            this.progressAnimation(1.0);
+        },
+        //进度动画
+        progressAnimation:function(progress){
+            const animation = weex.requireModule('animation')
+            var deviceWidth =WXEnvironment.deviceWidth
+            if(progress>1){
+                progress = 1.0
+            }else if(progress<0){
+                progress = 0;
+            }
+            let width = progress * deviceWidth;
+            var progressEl = this.$refs.progress;
+            animation.transition(progressEl, {
+            styles: {
+                width: width,
+            },
+            duration: 500, //ms
+            timingFunction: 'ease',
+            delay: 0 
+            }, function () {
+                if(progress == 1.0){
+                    animation.transition(progressEl, {
+                    styles: { width: 0 },
+                    duration: 0, 
+                    delay: 0 
+                    }, function () {})
+                }
+                
+            })
+        },
     }
 }
 </script>
@@ -43,6 +105,14 @@ export default {
     flex-direction: column;
     background-color: #ffffff;
     display: flex;
+}
+.progress {
+    position:absolute;
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 4px;
+    background-color: red;
 }
 .web {
     width: 750px;
